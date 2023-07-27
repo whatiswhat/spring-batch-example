bee128b06bf87ea3e80a86a3e0e4081d3d54944f
5109160176 <lin.li2@sony.com>
2023-07-27 21:54:13 +0800

    source commit

---
+++ src/main/java/com/example/home/demo3/mybatis/AccessingDataMyBatisApplication.java
@@ -0,0 +1,14 @@
+package com.example.home.demo3.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMyBatisApplication {
+
+    public static void main(String[] args) throws Exception {
+        System.exit(SpringApplication.exit(SpringApplication.run(AccessingDataMyBatisApplication.class, args)));
+//        SpringApplication.run(AccessingDataMysqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/home/demo3/mybatis/Demo3Repository.java
@@ -0,0 +1,9 @@
+package com.example.home.demo3.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Demo3Repository {
+
+    
+}
